959beb34901058579a5bf43f18dffebe9ba5d310
Dave Rand <david.rand@protolabs.co.uk>
2015-01-22 15:35:51 +0000

    taking care of largest

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,9 +6,21 @@ public class QueryProcessor {
         if (query.contains("name")) {
             return "MedhaDaveMike";
         }
-        else if
-            (query.contains("hi")) {
-             return "hello";
+
+        else if (query.contains("plus")) {
+            String[] values = query.split(" ");
+            if(values.length >4){
+            return Integer.parseInt(values[2]) + Integer.parseInt(values[4]) + "";
+            }
+            return "";
+        }
+        else if (query.contains("largest")) {
+            String[] result = query.split(": ");
+            if(result.length >2){
+                String values[] = result[2].split(", ");
+                return (Integer.parseInt(values[0])>Integer.parseInt(values[1])? values[0] : values[1] ) ;
+            }
+            return "";
         }
         return "";
     }
